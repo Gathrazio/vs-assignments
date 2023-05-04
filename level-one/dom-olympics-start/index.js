50b454c7ffa80a896cf10cb431a6b9048a4c9243
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-05-04 11:57:46 -0600

    completed bronze medal challenge

--- level-one/dom-olympics-start/index.js
+++ level-one/dom-olympics-start/index.js
@@ -1,1 +1,9 @@
-/* hello! */
+/* Bronze Medal Challenge: insert a header using JS */
+/* ------------------------------------------------ */
+
+titleHeader = document.createElement("h1");
+titleHeader.textContent = "Javascript Made This!";
+contributorInfoHeader = document.createElement("h6");
+contributorInfoHeader.innerHTML = "<span id='name'> Noah </span> wrote the JavaScript.";
+document.body.prepend(titleHeader)
+titleHeader.insertAdjacentElement("afterend", contributorInfoHeader)
