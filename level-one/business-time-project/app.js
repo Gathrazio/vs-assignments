c2d7ec19534e83bd483c30193ee28fd6785ff190
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-05-10 19:31:41 -0600

    started and completed (to the degree that I can at the moment) the business-time project and messed with the css-grid practice assignment

---
+++ level-one/business-time-project/app.js
@@ -0,0 +1,6 @@
+shopButton = document.getElementById("shop");
+homeButton = document.getElementById("home");
+
+shopButton.addEventListener("click", () => window.location.href = 'index2.html')
+
+homeButton.addEventListener("click", () => window.location.href = 'index.html')
