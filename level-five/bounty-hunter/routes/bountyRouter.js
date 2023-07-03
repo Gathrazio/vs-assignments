6cc70288739918f8d6ddf2501d06f2cf3e8b53fe
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-07-03 11:08:00 -0600

    completed bounty router assignment

--- level-five/bounty-hunter/routes/bountyRouter.js
+++ level-five/bounty-hunter/routes/bountyRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bountyRouter = express.Router();
 const {v4: uuidv4} = require('uuid');
 
-const bounties = [
+let bounties = [
     {
         fname: "Smokin'",
         lname: "Joe",
@@ -30,14 +30,44 @@ const bounties = [
 ];
 
 bountyRouter.route('/')
-    .get((_, res) => {
-        res.send(bounties)
+    .get((_, res) => { // get all
+        res.send(bounties) // sending all bounties
     })
-    .post((req, res) => {
-        const newBounty = req.body;
+    .post((req, res) => { // post one
+        const newBounty = req.body; // we will just hope req.body is in the correct format
         newBounty._id = uuidv4();
         bounties.push(newBounty)
-        res.send(`Successfully added ${newBounty.name} to the bounty list.`)
+        res.send(`Successfully added ${newBounty.fname} ${newBounty.lname} to the bounty list.`) // notification of successful post
+    })
+
+bountyRouter.route('/:bountyId')
+    .get((req, res) => { // get one by Id
+        res.send(bounties.find(bounty => bounty._id === req.params.bountyId))
+    })
+    .delete((req, res) => { // delete one by Id
+        let deletedFName;
+        let deletedLName;
+        bounties = bounties.filter(bounty => { // re-assigning bounties array to eliminate the need for messy splicing
+            if (bounty._id === req.params.bountyId) {
+                deletedFName = bounty.fname;
+                deletedLName = bounty.lname;
+            }
+            return bounty._id !== req.params.bountyId; // will return true for everything but the element that we are wanting to delete
+        })
+        res.send(`The bounty with id ${req.params.bountyId} (i.e., ${deletedFName} ${deletedLName}) was successfully removed.`) // notification of successful removal
+    })
+    .put((req, res) => { // update one by Id
+        bounties.every((bounty, index) => { // .every() array method will stop the interior for loop when a falsey value is returned on any particular iteration
+            if (bounty._id === req.params.bountyId) {
+                bounties.splice(index, 1, ({
+                    ...bounty,
+                    ...req.body
+                }))
+                res.send(bounties[index]) // returns updated entry from database
+                return false;
+            }
+            return true;
+        })
     })
 
 
--- level-five/bounty-hunter/server.js
+++ level-five/bounty-hunter/server.js
@@ -7,6 +7,4 @@ app.use(express.json()) // looks for a request body and turns it into req.body
 
 app.use('/bounties', require('./routes/bountyRouter.js'))
 
-app.listen(9000, () => {
-    console.log("listening")
-})
+app.listen(8999)
