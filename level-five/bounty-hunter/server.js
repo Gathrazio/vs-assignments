0d739324491a3cde4b9f89f54b421edca57c5486
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-07-11 17:23:57 -0600

    configured database so that get requests are functional

--- level-five/bounty-hunter/server.js
+++ level-five/bounty-hunter/server.js
@@ -2,18 +2,21 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 // middleware for every request
-
 app.use(express.json())
 app.use(morgan('dev'))
 
 //connect to db
-mongoose.set('strictQuery', true)
-mongoose.connect("mongodb://localhost:27017/bountiesdb", () => console.log('connected to database'))
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URL, () => console.log('connected to database'))
 
+// routes
 app.use('/api/bounties', require('./routes/bountyRouter.js'))
 
+
+// error handling
 app.use((err, req, res, next) => {
     return res.send({errMsg: err.message})
 })
