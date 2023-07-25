5609845a5c9af06186c03e8d222881d692e4d302
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-07-24 19:03:48 -0600

    added prompt to validate_dependency code challenge

--- level-six/code-challenges-solutions/validate_dependency.js
+++ level-six/code-challenges-solutions/validate_dependency.js
@@ -1,3 +1,15 @@
+/**
+ * @description - determine whether the list of tuples creates a valid dependency tree (no circular deps)
+ * @difficulty - 5/5
+ * 
+ * @param {Array<[string, string]>} - array of tuples representing a dependency between two things represented as strings (i.e.  ['A', 'B'] where B depends on A)
+ * @returns {Boolean}
+ * 
+ * @example - validateDependencies([['A','B'], ['B','C']]) // --> true
+ * @example - validateDependencies([['A','B'], ['B','A']]) // --> false (B depends on A, which already depends on B, this is circular)
+ */
+
+
 const validateDependencies = (deps) => {
 
     const graph = {};
