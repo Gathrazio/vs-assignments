1bae772f2564adb38b4e05e12f8f7eb91b593742
Noah Jensen <129221397+Gathrazio@users.noreply.github.com>
2023-07-27 17:20:50 -0600

    hashing

--- level-six/rtv/models/User.js
+++ level-six/rtv/models/User.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
 
 const userSchema = new Schema({
     username: {
@@ -18,5 +21,16 @@ const userSchema = new Schema({
         default: false
     }
 }, { timestamps: true })
+
+// pre-save hook to encrypt user passwords on signup
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next()
+    bcrypt.hash(user.password, saltRounds, (err, hash) => {
+        if (err) return next(err)
+        user.password = hash;
+        next()
+    })
+})
 
 module.exports = mongoose.model("User", userSchema);
